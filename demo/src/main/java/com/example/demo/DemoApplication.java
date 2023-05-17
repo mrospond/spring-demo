1229aa9aca8baf3d2993b414ca134f1eb300e752
mrospond <marekrospond@gmail.com>
2023-05-18 00:51:50 +0200

    Scheduling

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 /* if outside the module: */
 //import org.springframework.boot.autoconfigure.domain.EntityScan;
 //import org.springframework.context.annotation.ComponentScan;
 //import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableScheduling
 @SpringBootApplication
 public class DemoApplication {
 
